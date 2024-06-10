33e97836e9ca882e2b04bb13cbbe540f55db5e60
ma-zu <mattias.zurbuchen@noseryoung.ch>
2024-06-10 16:14:00 +0200

    Carousel Done

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace RezeptRadar
+﻿using RezeptRadar.Pages;
+
+namespace RezeptRadar
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute("RezeptSeite", typeof(RezeptSeite));
            
         }
     }
--- Pages/Browse.xaml.cs
+++ Pages/Browse.xaml.cs
@@ -1,10 +1,39 @@
+using RezeptRadar.Models;
+
 namespace RezeptRadar.Pages;
 
 public partial class Browse : ContentPage
 {
 	public Browse()
 	{
+		
 		InitializeComponent();
-	}
+
+        var itemsDaily = new List<CollectionItem>
+        {
+
+            new CollectionItem{Title="Tofu Edamame Bowl 1", Description= "Add 11 Ingredients", Image= "bowl.jpg"},
+            new CollectionItem{Title="Tofu Edamame Bowl 2", Description= "Add 11 Ingredients", Image= "bowl.jpg"},
+            new CollectionItem{Title="Tofu Edamame Bowl 3", Description= "Add 11 Ingredients", Image= "bowl.jpg"},
+            new CollectionItem{Title="Tofu Edamame Bowl 4", Description= "Add 11 Ingredients", Image= "bowl.jpg"},
+            new CollectionItem{Title="Tofu Edamame Bowl 5", Description= "Add 11 Ingredients", Image= "bowl.jpg"}
+        };
+        carouselViewDaily.ItemsSource = itemsDaily;
+        var itemsSpring = new List<CollectionItem>
+        {
+
+            new CollectionItem{Title="Tofu Edamame Bowl 1", Description= "Add 11 Ingredients", Image= "bowl.jpg"},
+            new CollectionItem{Title="Tofu Edamame Bowl 2", Description= "Add 11 Ingredients", Image= "bowl.jpg"},
+            new CollectionItem{Title="Tofu Edamame Bowl 3", Description= "Add 11 Ingredients", Image= "bowl.jpg"},
+            new CollectionItem{Title="Tofu Edamame Bowl 4", Description= "Add 11 Ingredients", Image= "bowl.jpg"},
+            new CollectionItem{Title="Tofu Edamame Bowl 5", Description= "Add 11 Ingredients", Image= "bowl.jpg"}
+        };
+        carouselViewSpring.ItemsSource = itemsSpring;
+    }
+    async void OnTappedRecepie (object sender, EventArgs e)
+    {
+        indicatorViewSpring.IndicatorColor = Color.FromRgb(255,255,255);
+        await Shell.Current.GoToAsync("RezeptSeite");
+    }
 
 }
