411ba12adfd5e9f4b0c90f3ac0a22a90ebaa9492
ma-zu <mattias.zurbuchen@noseryoung.ch>
2024-06-11 20:34:46 +0200

    Add Search, Radio, Slider, Checkbox

--- Pages/Search.xaml.cs
+++ Pages/Search.xaml.cs
@@ -5,5 +5,18 @@ public partial class Search : ContentPage
 	public Search()
 	{
 		InitializeComponent();
-	}
+        int value = (int)Math.Round(timeSlider.Value / 5) * 5;
+        timeSliderValue.Text = value.ToString() + " min";
+    }
+
+    private void search_Completed(object sender, EventArgs e)
+    {
+
+    }
+
+    private void timeSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        int value = (int) Math.Round(timeSlider.Value/5)*5 ;
+        timeSliderValue.Text = value.ToString()+ " min" ;
+    }
 }
