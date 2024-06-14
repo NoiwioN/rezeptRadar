b0fe08e3e3c0af7e3c07169f8c65107a603e348d
ma-zu <mattias.zurbuchen@noseryoung.ch>
2024-06-14 19:46:19 +0200

    Added Favorite Page

--- Pages/Favorites.xaml.cs
+++ Pages/Favorites.xaml.cs
@@ -1,3 +1,5 @@
+using RezeptRadar.Models;
+
 namespace RezeptRadar.Pages;
 
 public partial class Favorites : ContentPage
@@ -5,5 +7,26 @@ public partial class Favorites : ContentPage
 	public Favorites()
 	{
 		InitializeComponent();
-	}
+        var itemsDaily = new List<CollectionItem>
+        {
+
+            new CollectionItem{Title="Tofu Edamame Bowl 1", Description= "Add 11 Ingredients", Image= "bowl.jpg"},
+            new CollectionItem{Title="Tofu Edamame Bowl 2", Description= "Add 11 Ingredients", Image= "bowl.jpg"},
+            new CollectionItem{Title="Tofu Edamame Bowl 3", Description= "Add 11 Ingredients", Image= "bowl.jpg"},
+            new CollectionItem{Title="Tofu Edamame Bowl 4", Description= "Add 11 Ingredients", Image= "bowl.jpg"},
+            new CollectionItem{Title="Tofu Edamame Bowl 5", Description= "Add 11 Ingredients", Image= "bowl.jpg"}
+        };
+        carouselViewDaily.ItemsSource = itemsDaily;
+    }
+    private async void recipieClicked(object sender, TappedEventArgs e)
+    {
+        Routing.RegisterRoute("recipiePage", typeof(RezeptSeite));
+        await Shell.Current.GoToAsync("recipiePage");
+
+    }
+
+    private void buttonTypeBeat_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
